824855330ed6545f1d3c750628264f0d2cc255a5
jp-suzuki <suzukij.2000@gmail.com>
2024-02-01 20:26:49 +0900

    fix: 項の最初に0が入らない不具合の修正

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let displayFormula = "";
-let lastInput = "default";  //
+let displayFormula = "0";
+let lastInput = "zero";  //
 let hasPoint = false;
 let lastChar = "";
-let nowTermLength = 0;
+let nowTermLength = 1;
 
 //数値表示領域の更新
 function refreshDisplay(){
@@ -11,8 +11,9 @@ function refreshDisplay(){
 //数値表示領域の初期化
 function resetDisplay(){
   displayFormula = "";
-  lastInput = "default";
+  lastInput = "zero";
   hasPoint = false;
+  let nowTermLength = 1;
   $(".calculator-display").text("0");
 }
 //引数である文字列のうち、一番最後の文字を返す
@@ -29,7 +30,7 @@ function deleteLastTerm(){
     deleteLastChar();
   }
   nowTermLength = 0;
-  if(displayFormula == ""){
+  if(displayFormula == "0"){
     resetDisplay();
   }else{
     lastInput = "operator";
@@ -55,7 +56,7 @@ function executeCalculation(){
   displayFormula = eval(displayFormula);
   refreshDisplay();
   displayFormula = "";
-  lastInput = "default";
+  lastInput = "zero";
   hasPoint = false;
 }
 
@@ -64,6 +65,10 @@ function addInputNumber(inputContents){
   if(checkFormulaLength(15)){
     ; //何もしない
   }else{
+    if(lastInput == "zero"){
+      nowTermLength -= 1;
+      deleteLastChar();
+    }
     displayFormula += inputContents;
     lastInput = "number";
     nowTermLength += 1;
@@ -71,20 +76,32 @@ function addInputNumber(inputContents){
   }
 }
 function addInputZero(inputContents){
-  if(checkFormulaLength(15) || lastInput == "default" || lastInput == "operator"){
+  if(checkFormulaLength(15) || lastInput == "zero"){
     ; //何もしない
   }else if(inputContents == "00"){
-    //00ボタンを押したときに14桁から16桁にならないよう制限
-    if(checkFormulaLength(14)){
+    //項の最初の0は数字で上書きできるように
+    if(nowTermLength == 0){
       displayFormula += inputContents;
       deleteLastChar();
       nowTermLength += 1;
+      lastInput = "zero";
+    }
+    //00ボタンを押したときに14桁から16桁にならないよう制限
+    else if(checkFormulaLength(14)){
+      displayFormula += inputContents;
+      deleteLastChar();
+      nowTermLength += 1;
+      lastInput = "number";
     }else{
       displayFormula += inputContents;
       nowTermLength += 2;
+      lastInput = "number";
     }
-    lastInput = "number";
     refreshDisplay();
+  }else if(nowTermLength == 0){
+    displayFormula += inputContents;
+    nowTermLength += 1;
+    lastInput = "zero";
   }else{
     displayFormula += inputContents;
     lastInput = "number";
@@ -97,7 +114,7 @@ function addInputPoint(inputContents){
     ; //何もしない
   }
   //整数を省略して小数点を押したとき、自動で[0.]に補正する（電卓の挙動再現）
-  else if(lastInput == "default" || lastInput == "operator"){
+  else if(lastInput == "zero" || lastInput == "operator"){
     displayFormula += "0" + inputContents;
     lastInput = "point";
     hasPoint = true;
@@ -113,7 +130,7 @@ function addInputPoint(inputContents){
 }
 function addInputOperator(inputContents){
   lastChar = getLastChar(displayFormula);
-  if(checkFormulaLength(14) || lastInput == "default"){
+  if(checkFormulaLength(14) || lastInput == "zero"){
     ; //何もしない
   }
   //四則演算子を連続で入力したとき、最後の入力のみを適用する（電卓の挙動再現）
